f3c87c99717ed6f4545de0b36d0733857fac7b78
JuanJoseArGo <juanjo100621@hotmail.com>
2022-08-01 21:14:53 -0500

    Proyecto DW subida 3

--- conexion.php
+++ conexion.php
@@ -1,9 +1,14 @@
 <?php
+// Pa probar papi
+//$servidor = "localhost";
+//$usuario = "root";
+//$contrasenia = "";
+//$BD = "mascotas";
 
-$servidor = "localhost";
-$usuario = "root";
-$contrasenia = "";
-$BD = "mascotas";
+$servidor = "remotemysql.com";
+$usuario = "pn4Ka2jL80";
+$contrasenia = "Osb60Li89G";
+$BD = "pn4Ka2jL80";
 
 $conexion = mysqli_connect($servidor, $usuario, $contrasenia, $BD);
 
